539d9658479ef7dd5ef06afe041c9b416c7d049b
olgunbey <olgunsahin0161@hotmail.com>
2022-10-22 02:11:38 +0300

    Güncel Kitap stok

---
+++ Ornek11/Migrations/20221019110409_Mig_1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ornek11.Migrations
+{
+    public partial class Mig_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kitaplar",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitapAD = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kitaplar", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KitapAciklama",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    KitapAcciklama = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitapAciklama", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_KitapAciklama_Kitaplar_ID",
+                        column: x => x.ID,
+                        principalTable: "Kitaplar",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KitaplarFiyatOZ",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    KitapFiyat = table.Column<int>(type: "int", nullable: false),
+                    KitapStok = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitaplarFiyatOZ", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_KitaplarFiyatOZ_Kitaplar_ID",
+                        column: x => x.ID,
+                        principalTable: "Kitaplar",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KitapAciklama");
+
+            migrationBuilder.DropTable(
+                name: "KitaplarFiyatOZ");
+
+            migrationBuilder.DropTable(
+                name: "Kitaplar");
+        }
+    }
+}
---
+++ Ornek11/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+{
+    DBXContext context = new();
+
+    //Kitaplar kitap = new()
+    //{
+    //    KitapAD = "Sefiller",
+    //    KitapAciklama = new()
+    //    { KitapAcciklama = "Değerli bir kitap2" },
+    //    KitaplarFıyatOZ = new() { KitapFiyat = 50, KitapStok = 2 }
+    //};
+    //context.Kitaplar.Add(kitap);
+    //context.SaveChanges();
+
+
+    //SILME IŞLEMI GERCEKLESTIRME
+    Kitaplar kitaplar = context.Kitaplar.Include(p => p.KitaplarFıyatOZ).Include(p => p.KitapAciklama).FirstOrDefault(p => p.ID == 3);
+
+    if (kitaplar.KitaplarFıyatOZ.KitapStok == 1)
+    {
+        context.KitaplarFiyatOZ.Remove(kitaplar.KitaplarFıyatOZ);
+        context.Kitaplar.Remove(kitaplar);
+        context.KitapAciklama.Remove(kitaplar.KitapAciklama);
+        context.SaveChanges();
+    }
+    else
+    {
+        kitaplar.KitaplarFıyatOZ.KitapStok -= 1;
+        context.SaveChanges();
+    }
+
+
+
+}
+
+
+public class Kitaplar
+{
+    public int ID { get; set; }
+    public string KitapAD { get; set; }
+    public KitaplarFiyatOZ KitaplarFıyatOZ { get; set; }
+    public KitapAciklama KitapAciklama { get; set; }
+}
+public class KitaplarFiyatOZ
+{
+    public int ID { get; set; }
+    public int KitapFiyat { get; set; }
+    public int KitapStok { get; set; }
+    public Kitaplar Kitaplar { get; set; }
+}
+public class KitapAciklama
+{
+    public int ID { get; set; }
+    public string KitapAcciklama { get; set; }
+    public Kitaplar Kitaplar { get; set; }
+}
+
+public class DBXContext:DbContext
+{
+    public DbSet<Kitaplar> Kitaplar { get; set; }
+    public DbSet<KitapAciklama> KitapAciklama { get; set; }
+    public DbSet<KitaplarFiyatOZ> KitaplarFiyatOZ { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDb; Initial Catalog=Kitaplık");
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Kitaplar>()
+             .HasKey(p => p.ID);
+
+        modelBuilder.Entity<KitapAciklama>()
+            .HasKey(p => p.ID);
+
+        modelBuilder.Entity<KitaplarFiyatOZ>()
+            .HasKey(p => p.ID);
+
+        modelBuilder.Entity<KitaplarFiyatOZ>()
+              .HasOne(p => p.Kitaplar)
+              .WithOne(p => p.KitaplarFıyatOZ)
+              .HasForeignKey<KitaplarFiyatOZ>(p => p.ID);
+
+        modelBuilder.Entity<KitapAciklama>()
+            .HasOne(p=>p.Kitaplar)
+            .WithOne(p=>p.KitapAciklama)
+            .HasForeignKey<KitapAciklama>(p => p.ID);
+    }
+}
